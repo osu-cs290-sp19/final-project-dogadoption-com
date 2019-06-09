19c2272ae57a7ccf3ac71640ca7575d15cc07cc4
Brian Kim <briankim@10-197-34-222.sds.oregonstate.edu>
2019-06-08 20:15:05 -0700

    Styles

--- public/postTemplate.js
+++ public/postTemplate.js
@@ -5,15 +5,15 @@ templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(containe
 
   return "<article class=\"post\">\n\n    <div class=\"picture\">\n        <img src=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\" class=\"dog-photo-image\"/>\n    </div>\n    <div class=\"post-content\">\n        <p class=\"name\">\n            Name: "
+    + "\" class=\"dog-photo-image\"/>\n    </div>\n    <div class=\"post-content\">\n        <p class=\"name description-text\">\n            Name: "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "\n        </p>\n        <p class=\"breed\">\n            Breed: "
+    + "\n        </p>\n        <p class=\"breed description-text\">\n            Breed: "
     + alias4(((helper = (helper = helpers.breed || (depth0 != null ? depth0.breed : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"breed","hash":{},"data":data}) : helper)))
-    + "\n        </p>\n        <p class= \"price\">\n            Adoption Fee: "
+    + "\n        </p>\n        <p class= \"price description-text\">\n            Adoption Fee: $"
     + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
-    + "\n        </p>\n        <p class= \"gender\">\n            Gender: "
+    + "\n        </p>\n        <p class= \"gender description-text\">\n            Gender: "
     + alias4(((helper = (helper = helpers.gender || (depth0 != null ? depth0.gender : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"gender","hash":{},"data":data}) : helper)))
-    + "\n        </p>\n        <p class=\"description\">\n            Description: "
+    + "\n        </p>\n        <p class=\"description description-text\">\n            Description: "
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "\n        </p>\n    </div>\n</article>\n";
 },"useData":true});
